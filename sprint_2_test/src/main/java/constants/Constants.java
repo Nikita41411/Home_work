320bc3f951a0aa6a64a8f7896cc21438ff58b66c
Kukushkin Nikita <kukushkin.n.a@yandex.ru>
2025-09-25 08:32:29 +0300

    удалил ген3

--- sprint_2_test/src/main/java/constants/Constants.java
+++ sprint_2_test/src/main/java/constants/Constants.java
@@ -4,6 +4,5 @@ public class Constants {
 
     public static final String GENERATION_1 = "gen1";
     public static final String GENERATION_2 = "gen2";
-    public static final String GENERATION_3 = "gen3";
     public static final int SOYUZ_GENERATION_1_MONTHS_RISK = 2;
 }
