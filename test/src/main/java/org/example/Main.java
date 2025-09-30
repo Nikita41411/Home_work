003a3f05c675c7ce1df034c2a2ea579adb19af01
Nikita Kukushkin <kukushkin.n.a@yandex.ru>
2025-09-30 15:20:10 +0300

    new_test

--- test/src/main/java/org/example/Main.java
+++ test/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Human person1 = new Human("gf", 400);
+        Human person1 = new Human("Вася", 22);
         person1.speak();
     }
 }
