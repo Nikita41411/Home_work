cc9a4b1415524527020dee8979da62284e4c992b
Nikita Kukushkin <kukushkin.n.a@yandex.ru>
2025-10-10 11:11:46 +0300

    Сохраняю

--- test/src/main/java/org/example/Main.java
+++ test/src/main/java/org/example/Main.java
@@ -2,7 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Human person1 = new Human("Вася", 22);
-        person1.speak();
+        Human p1 = new Human("Вася", 21);
+        p1.speak();
+        Human p2 = new Human("Петя", 14);
+        String v = p2.getName();
+        String upperCase = v.toUpperCase();
+        System.out.printf(upperCase + " ");
+        p2.speak();
     }
 }
